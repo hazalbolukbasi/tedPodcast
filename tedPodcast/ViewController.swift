a8b6ab7fedca241cef7e4651e641cc3c82548b8d
hazalbolukbasi <hazalbolukbasi@icloud.com>
2024-10-02 00:39:27 +0300

    Initial Commit

---
+++ tedPodcast/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  tedPodcast
+//
+//  Created by Hazal Bölükbaşı on 2.10.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
